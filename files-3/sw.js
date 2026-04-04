7ec768b10353ae487a3b5fb5eaf03fec0971ffa6
carlosgranderodriguez-a11y <carlos.grande.rodriguez@gmail.com>
2026-04-04 10:58:43 +0200

    Add files via upload

---
+++ files-3/sw.js
@@ -0,0 +1,38 @@
+const CACHE = "esgrima-v1";
+const ASSETS = [
+  "./index.html",
+  "./espada_masculina_2025-26.html",
+  "./espada_femenina_2025-26.html",
+  "./florete_masculino_2025-26.html",
+  "./florete_femenino_2025-26.html",
+  "./sable_masculino_2025-26.html",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  // Network first for Google Sheets data, cache first for assets
+  if(e.request.url.includes("google") || e.request.url.includes("script.google")){
+    e.respondWith(fetch(e.request).catch(() => new Response("")));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
